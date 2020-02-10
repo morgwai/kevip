fbc59dcc139098fae0b929c05a113bc5a84e2e96
Piotr Morgwai Kotarbinski <git@morgwai.pl>
2020-02-10 23:16:55 +0700

    revert to appending, give up on MASQUERADE

--- vip-add.sh
+++ vip-add.sh
@@ -5,12 +5,12 @@ maskTarget=( ${3} ) && # for example "24 10.33.0.4"
 mask=${maskTarget[0]} &&
 target=${maskTarget[1]} &&
 ipCmd=add &&
-iptablesCmd='-I' &&
+iptablesCmd='-A' &&
 if [[ ${0} == *vip-del.sh ]]; then
 	ipCmd=del &&
 	iptablesCmd='-D' ;
 fi &&
 
 ip addr ${ipCmd} ${vip}/${mask} dev ${iface} &&
-iptables -t nat ${iptablesCmd} PREROUTING -d ${vip} -j DNAT --to-destination ${target} &&
-iptables -t nat ${iptablesCmd} POSTROUTING -d ${target} -j MASQUERADE
+iptables -t nat ${iptablesCmd} PREROUTING -d ${vip} -j DNAT --to-destination ${target}
+# && iptables -t nat ${iptablesCmd} POSTROUTING -d ${target} -j MASQUERADE
