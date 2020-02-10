7d4abef7e8ec2d01ee60d2a5e2d3b7ed5204a600
Piotr Morgwai Kotarbinski <git@morgwai.pl>
2020-02-10 21:59:08 +0700

    make vip-del link to vip-add, more spercifc masquarade

--- vip-add.sh
+++ vip-add.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
-maskTarget=( ${3} ) &&
+iface=${1} &&
+vip=${2}
+maskTarget=( ${3} ) && # for example "24 10.33.0.4"
 mask=${maskTarget[0]} &&
 target=${maskTarget[1]} &&
+ipCmd=add &&
+iptablesCmd='-A' &&
+if [[ ${0} == *vip-del.sh ]]; then
+	ipCmd=del &&
+	iptablesCmd='-D' ;
+fi &&
 
-ip addr add ${2}/${mask} dev ${1} &&
-iptables -t nat -A PREROUTING -d ${2} -j DNAT --to-destination ${target} &&
-iptables -t nat -A POSTROUTING -j MASQUERADE
+ip addr ${ipCmd} ${vip}/${mask} dev ${iface} &&
+iptables -t nat ${iptablesCmd} PREROUTING -d ${vip} -j DNAT --to-destination ${target} &&
+iptables -t nat ${iptablesCmd} POSTROUTING -d ${target} -j MASQUERADE
--- vip-del.sh
+++ vip-del.sh
@@ -1,8 +1,1 @@
-#!/bin/bash
-maskTarget=( ${3} ) &&
-mask=${maskTarget[0]} &&
-target=${maskTarget[1]} &&
-
-ip addr del ${2}/${mask} dev ${1} &&
-iptables -t nat -D PREROUTING -d ${2} -j DNAT --to-destination ${target} &&
-iptables -t nat -D POSTROUTING -j MASQUERADE
+vip-add.sh
